e34940da2546ee6e19c878db06723970065c0df5
flga <flga@users.noreply.github.com>
2019-08-28 14:57:58 +0100

    automate tag management

---
+++ update.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+cd src/freetype-tip
+git pull
+cd ../..
+if [[ ! `git status --porcelain` ]]; then
+    exit
+fi
+
+git tag -d tip
+git push origin :refs/tags/tip
+git add src
+git commit -m "update tip"
+git tag tip
+# git push --follow-tags
